b2f05c0731548a9cc62059631588340355200b4c
LMichalczak <218718@student.pwr.edu.pl>
2019-01-10 20:25:58 +0100

    computation of feature importance - add averaging and bug fixed

--- remove_eigenface_feature_selector.py
+++ remove_eigenface_feature_selector.py
@@ -22,16 +22,21 @@ class RemoveEigenfaceFutureSelector:
         selector = [x for x in range(self.number_of_vectors) if x != vector_number]
         inputs_leave_one_out = self.inputs_after_pca[:, selector]
 
-        X_train, X_test, y_train, y_test = train_test_split(inputs_leave_one_out, self.target, test_size=0.25)
+        accumulated_effectiveness = 0
+        iterations = 5
+        for i in range(iterations):
+            x_train, x_test, y_train, y_test = train_test_split(inputs_leave_one_out, self.target, test_size=0.25)
 
-        dtc = DecisionTree()
-        dtc.learn(X_train, y_train)
-        y_test_dtc_pred = dtc.predict(X_test)
-        return self.compute_effectiveness(y_test, y_test_dtc_pred)
+            dtc = DecisionTree()
+            dtc.learn(x_train, y_train)
+            y_test_dtc_pred = dtc.predict(x_test)
+            accumulated_effectiveness = accumulated_effectiveness + self.compute_effectiveness(y_test, y_test_dtc_pred)
+
+        return accumulated_effectiveness/iterations
 
     def compute_effectiveness(self, ideal, real):
         accuracy = accuracy_score(ideal, real)
         loss = hamming_loss(ideal, real)
-        # f1 = f1_score(ideal, real, average='macro') # error
-        # return 1 - ((accuracy + (1-loss) + f1) / 3)
-        return 1 - ((accuracy + (1-loss)) / 2)
+        # f1 = f1_score(ideal, real, average='macro') # warning
+        # return (accuracy + (1-loss) + f1) / 3
+        return (accuracy + (1-loss)) / 2
